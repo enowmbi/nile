84e3fe0e98c6b79c0dbe83dc5002a21ea09fc406
Enow Mbi <benowmbi@yahoo.com>
2021-11-30 13:30:24 +0100

    Add dependent destroy to has_many association between author and books

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,8 @@
 class Author < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: true }
 end
