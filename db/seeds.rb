daad51c84675594c0a9fde205c9b409c5b07d8dd
Enow Mbi <benowmbi@yahoo.com>
2021-11-30 11:54:18 +0100

    Fix failures in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-100.times do
-  Book.create({
-    title: Faker::Lorem.unique.paragraph(sentence_count: 1),
-    author: Faker::Name.name,
-    published_date: Date.today
-  })
+Author.transaction do
+  10.times do
+    Author.create({
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email:  Faker::Internet.unique.email
+    })
+  end
+
+  author_ids = Author.ids
+
+  100.times do
+    Book.create({
+      title: Faker::Lorem.unique.paragraph(sentence_count: 1),
+      author_id: author_ids.sample,
+      published_date: Date.today
+    })
+  end
 end
