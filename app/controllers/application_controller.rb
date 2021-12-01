238cce79fff5bf0571a9c0ecd642ba7ce8da7c0f
Enow Mbi <benowmbi@yahoo.com>
2021-12-01 09:22:32 +0100

    Handle parameter missing error with rescue_with

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
-      rescue_from ActiveRecord::RecordNotFound, with: :display_record_not_found_error_message
+  rescue_from ActiveRecord::RecordNotFound, with: :display_record_not_found_error_message
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+
+  private
+
+  def parameter_missing
+    render json: {error: "Parameter missing for #{params[:action]} action of #{params[:controller]} controller"}, status: :not_found
+  end
 end
